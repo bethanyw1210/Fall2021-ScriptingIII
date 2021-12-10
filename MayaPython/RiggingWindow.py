2a7eba55871f9e10af8e17195e48bf901c8ece2c
Bethany Ylst <bethany.w1210@gmail.com>
2021-12-10 16:23:53 -0700

    finished UI on rigging window, just need to fix rename tool

--- MayaPython/RiggingWindow.py
+++ MayaPython/RiggingWindow.py
@@ -49,11 +49,83 @@ def createAndGroupControl(input):
 
 
 
-window = cmds.window("Rigging Toolkit", height=250, width=250)
+window = cmds.window("Rigging Toolkit", height=250, width=400
+                     )
 cmds.columnLayout(parent=window)
-renameTextButton = cmds.textField("Rename", placeholderText="Joint Name", height=25, width=250)
-renameButton = cmds.button("Rename Joints", width=250, command="Rename('Leg_##_Jnt', 1")
-colorIndexButton = cmds.textField("Color", placeholderText="Which Color?", height=25, width=250)
-controlButton = cmds.button("Create Control", width=250, command="createAndGroupControl(13)")
-assignColor = cmds.button("Assign Color", width=250, command="colorChanger(13)")
+cmds.rowColumnLayout(numberOfColumns = 2)
+
+renameText = cmds.textField("Rename", placeholderText="Joint Name",
+                                  height=50, width=200)
+renameButton = cmds.button("Rename Joints", height=50, width=200,
+                           command="Rename('Leg_##_Jnt', 1")
+
+controlButton = cmds.button("Create Control", height=50, width=200,
+                            command="createAndGroupControl(13)")
+
+##Colors
+assignColorGray = cmds.button("Default (0)", backgroundColor = (.273,.273,.273), width=100,
+                          command="colorChanger(0)")
+assignColorBlack = cmds.button("Black (1)", backgroundColor = (0,0,0), width=100,
+                          command="colorChanger(1)")
+assignColorDarkGray = cmds.button("Dark Gray (2)", backgroundColor = (.113,.113,.113), width=100,
+                          command="colorChanger(2)")
+assignColorLightGray = cmds.button("Light Gray (3)", backgroundColor = (.273,.273,.273), width=100,
+                          command="colorChanger(3)")
+assignColorDarkRed = cmds.button("Dark Red (4)", backgroundColor = (.348,.057,.064), width=100,
+                          command="colorChanger(4)")
+assignColorDarkBlue = cmds.button("Dark Blue (5)", backgroundColor = (.029,.016,.186), width=100,
+                          command="colorChanger(5)")
+assignColorBlue = cmds.button("Blue (6)", backgroundColor = (.100,.031,3.658), width=100,
+                          command="colorChanger(6)")
+assignColorDarkGreen = cmds.button("Dark Green (7)", backgroundColor = (.059,.121,.045), width=100,
+                          command="colorChanger(7)")
+assignColorPurple = cmds.button("Purple (8)", backgroundColor = (.052,.013,.114), width=100,
+                          command="colorChanger(8)")
+assignColorMagent = cmds.button("Magenta (9)", backgroundColor = (.531,.091,.768), width=100,
+                          command="colorChanger(9)")
+assignColorBrown = cmds.button("Brown (10)", backgroundColor = (.288,.145,.088), width=100,
+                          command="colorChanger(10)")
+assignColorDarkBrown = cmds.button("Dark Brown (11)", backgroundColor = (.096,.058,.049), width=100,
+                          command="colorChanger(11)")
+assignColorSienna = cmds.button("Sienna (12)", backgroundColor = (.333,.089,.016), width=100,
+                          command="colorChanger(12)")
+assignColorAxisRed = cmds.button("Axis Red (13)", backgroundColor = (1.240,.106,.018), width=100,
+                          command="colorChanger(13)")
+assignColorAxisGreen = cmds.button("Axis Green (14)", backgroundColor = (.294,5.113,0), width=100,
+                          command="colorChanger(14)")
+assignColorAxisBlue = cmds.button("Axis Blue (15)", backgroundColor = (.081,.115,.404), width=100,
+                          command="colorChanger(15)")
+assignColorWhite = cmds.button("White (16)", backgroundColor = (16.3,16.3,16.3), width=100,
+                          command="colorChanger(16)")
+assignColorYellow = cmds.button("Yellow (17)", backgroundColor = (6.71,12.505,0), width=100,
+                          command="colorChanger(17)")
+assignColorLightBlue = cmds.button("Light Blue (18)", backgroundColor = (.432,1.213,8.659), width=100,
+                          command="colorChanger(18)")
+assignColorPastelTurquoise = cmds.button("Pastel Turquoise (19)", backgroundColor = (.351,5.519,.408), width=100,
+                          command="colorChanger(19)")
+assignColorPalePink = cmds.button("Pale Pink (20)", backgroundColor = (2.320,.606,.574), width=100,
+                          command="colorChanger(20)")
+assignColorFlesh = cmds.button("Flesh (21)", backgroundColor = (1.094,.572,.285), width=100,
+                          command="colorChanger(21)")
+assignColorPaleYellow = cmds.button("Pale Yellow (22)", backgroundColor = (7.271,12.877,0), width=100,
+                          command="colorChanger(22)")
+assignColorDarkTurquoise = cmds.button("Dark Turquoise (23)", backgroundColor = (.175,.402,.174), width=100,
+                          command="colorChanger(23)")
+assignColorLightBrown = cmds.button("Light Brown (24)", backgroundColor = (.384,.237,.089), width=100,
+                          command="colorChanger(24)")
+assignColorSage = cmds.button("Sage (25)", backgroundColor = (.443,.469,.104), width=100,
+                          command="colorChanger(25)")
+assignColorGreen = cmds.button("Green (26)", backgroundColor = (.286,.455,.104), width=100,
+                          command="colorChanger(26)")
+assignColorTurquoise = cmds.button("Turquoise (27)", backgroundColor = (.210,.448,.198), width=100,
+                          command="colorChanger(27)")
+assignColorRobinBlue = cmds.button("Robin Blue (28)", backgroundColor = (.223,.450,.473), width=100,
+                          command="colorChanger(28)")
+assignColorFrenchBlue = cmds.button("French Blue (29)", backgroundColor = (.147,.211,.456), width=100,
+                          command="colorChanger(29)")
+assignColorViolet = cmds.button("Violet (30)", backgroundColor = (.208,.096,.447), width=100,
+                          command="colorChanger(30)")
+assignColorPlum = cmds.button("Plum (31)", backgroundColor = (.350,.109,.219), width=100,
+                          command="colorChanger(31")
+
 cmds.showWindow(window)
